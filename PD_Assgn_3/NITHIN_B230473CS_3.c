b495aade6e32e80a123b3994f5a3f5158009f85a
Nithin Raj <nithinraj0404@gmail.com>
2024-03-19 14:17:38 +0530

    MaxBeautification of Q3

--- PD_Assgn_3/NITHIN_B230473CS_3.c
+++ PD_Assgn_3/NITHIN_B230473CS_3.c
@@ -9,60 +9,11 @@ struct Node{
 
 typedef struct Node * node;
 
-node createNode(int data){
-    node temp = (node)malloc(sizeof(struct Node));
-    temp->data = data;
-    temp->next = NULL;
-    return temp;
-}
-
-void insert(node *head, int data){
-    node temp = createNode(data);
-    if(*head == NULL){
-        *head = temp;
-        return;
-    }
-    temp->next = *head;
-    *head = temp;
-}
-
-int pop(node *head){
-    if(*head == NULL){
-        return -1;
-    }
-    node temp = *head;
-    *head = (*head)->next;
-    free(temp);
-    return 0;
-}
-
-char* dynamicString(){
-    char c;
-    char *s = (char *)malloc(1);
-    int size = 1;
-
-    scanf("%c", &c);
-
-    while(c != '\n'){
-        s[size-1] = c;
-        size++;
-        s = (char*)realloc(s, size);
-        scanf("%c", &c);
-    }
-    s[size-1] = '\0';
-
-    return s;
-}
-
-int length(node head){
-    node x = head;
-    int count = 0;
-    while(x != NULL){
-        count++;
-        x = x->next;
-    }
-    return count;
-}
+node createNode(int data);
+void insert(node *head, int data);
+int pop(node *head);
+char* dynamicString();
+int length(node head);
 
 int main(){
 
@@ -101,4 +52,60 @@ int main(){
     printf("%d\n", count);
 
     return 0;
+}
+
+node createNode(int data){
+    node temp = (node)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+
+void insert(node *head, int data){
+    node temp = createNode(data);
+    if(*head == NULL){
+        *head = temp;
+        return;
+    }
+    temp->next = *head;
+    *head = temp;
+}
+
+int pop(node *head){
+    if(*head == NULL){
+        return -1;
+    }
+    node temp = *head;
+    *head = (*head)->next;
+    free(temp);
+    return 0;
+}
+
+char* dynamicString(){  //proprietory
+
+    char c;
+    char *s = (char *)malloc(1);
+    int size = 1;
+
+    scanf("%c", &c);
+
+    while(c != '\n'){
+        s[size-1] = c;
+        size++;
+        s = (char*)realloc(s, size);
+        scanf("%c", &c);
+    }
+    s[size-1] = '\0';
+
+    return s;
+}
+
+int length(node head){
+    node x = head;
+    int count = 0;
+    while(x != NULL){
+        count++;
+        x = x->next;
+    }
+    return count;
 }
